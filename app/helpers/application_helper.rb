886d5df7582f282e47df8b587c024684a43a9789
Eduardo Gutierrez <eduardo@vermonster.com>
2014-06-09 17:30:19 -0400

    Cleanup `<head>` section of application layout
    * Use `asset_url` instead of hard-coded URLs.
    * Reduce duplication for `<link>` elements for Apple devices.
    * Consistent ruby 1.9 hash syntax where applicable
    * Consistent curly-brace spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  APPLE_TOUCH_ICONS = %w( 57 72 114 144 ).map do |size|
+    ["#{size}x#{size}", "apple-touch-icon-#{size}x#{size}.png"]
+  end
+
+  def apple_touch_icons
+    APPLE_TOUCH_ICONS
+  end
 end
