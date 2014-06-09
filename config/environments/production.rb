f94dba682679ed81f91b9770ca87b246f94e8574
Eduardo Gutierrez <eduardo@vermonster.com>
2014-06-09 17:19:14 -0400

    Remove deprecated `config.threadsafe!`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,6 @@ Wmsb::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Enable threaded mode
-  config.threadsafe!
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
